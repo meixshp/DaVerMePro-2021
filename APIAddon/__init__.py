4bf3493aad0fac576fb5684ef4f7ea18cf70cb57
mei.xyz <mei.xyz@web.de>
2021-12-10 23:19:29 +0100

    ✨ show champ name instead of id

--- APIAddon/__init__.py
+++ APIAddon/__init__.py
@@ -28,10 +28,8 @@ bl_info = {
 }
 
 
-
-
 class account(object):
-    def __init__(self,id,accountId,puuid,name,profileIconId,revisionDate,summonerLevel):
+    def __init__(self, id, accountId, puuid, name, profileIconId, revisionDate, summonerLevel):
         self.id = id
         self.accountId = accountId
         self.puuid = puuid
@@ -39,9 +37,10 @@ class account(object):
         self.profileIconId = profileIconId
         self.revisionDate = revisionDate
         self.summonerLevel = summonerLevel
-    
+
+
 class summoner(object):
-    def __init__(self, championId, championLevel, championPoints, lastPlayTime, championPointsSinceLastLevel, championPointsUntilNextLevel, chestGranted, tokensEarned, summonerId ):
+    def __init__(self, championId, championLevel, championPoints, lastPlayTime, championPointsSinceLastLevel, championPointsUntilNextLevel, chestGranted, tokensEarned, summonerId):
         self.championId = championId
         self.championLevel = championLevel
         self.championPoints = championPoints
@@ -51,6 +50,7 @@ class summoner(object):
         self.chestGranted = chestGranted
         self.tokensEarned = tokensEarned
         self.summonerId = summonerId
+
 
 class APIAddon(bpy.types.Operator):
 
@@ -69,23 +69,23 @@ class APIAddon(bpy.types.Operator):
         "category": "Add Mesh",
     }
     bl_options = {"REGISTER", "UNDO"}
-    num : bpy.props.IntProperty(
-        name = "Cube Number",
-        description = "Defines the number of cubes",
-        default = 5)
-        
-    cube_size_min : bpy.props.FloatProperty(
-        name = "Min Cube Size",
-        default = 0.5)
-        
-    cube_size_max : bpy.props.FloatProperty(
-        name = "Max Cube Size",
-        default = 2.0)
-        
-    cube_color : bpy.props.FloatVectorProperty(
-        name = "My Vector",
-        default = (0,0,1),
-        subtype = "COLOR")
+    num: bpy.props.IntProperty(
+        name="Cube Number",
+        description="Defines the number of cubes",
+        default=5)
+
+    cube_size_min: bpy.props.FloatProperty(
+        name="Min Cube Size",
+        default=0.5)
+
+    cube_size_max: bpy.props.FloatProperty(
+        name="Max Cube Size",
+        default=2.0)
+
+    cube_color: bpy.props.FloatVectorProperty(
+        name="My Vector",
+        default=(0, 0, 1),
+        subtype="COLOR")
 
     @classmethod
     def poll(cls, context):
@@ -100,80 +100,86 @@ class APIAddon(bpy.types.Operator):
         summonerName = "veryfirstghost"
         requestString = f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}"
         header = {
-            'X-Riot-Token': 'RGAPI-8224790b-11c8-4cf9-85fa-c6da6f120581',
+            'X-Riot-Token': 'RGAPI-30fdcbcc-b248-4c4e-a9c8-99dade527d87',
         }
 
         resp = requests.get(requestString, headers=header)
-        #print(resp.json())
+        # print(resp.json())
 
-        #print("help")
-        #print(resp.json())
-        #print("test")
-        #resp.decode('utf-8')
+        # print("help")
+        # print(resp.json())
+        # print("test")
+        # resp.decode('utf-8')
         #summunor_dict = resp.json()
         #summunor_obj = Summoner(**summunor_dict)
         s = account(**resp.json())
 
         print(f"name: {s.name}")
         print(f"id: {s.id}")
-        
 
         requestChampionMasteryString = f"https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{s.id}"
         resp = requests.get(requestChampionMasteryString, headers=header)
-        #print(resp.json())
+        # print(resp.json())
 
         requestChampionNames = f"http://ddragon.leagueoflegends.com/cdn/11.20.1/data/en_US/champion.json"
         respNames = requests.get(requestChampionNames)
-        #print(respNames.json())
+        # print(respNames.json())
 
         championInfo = json.loads(respNames.text)
-
-        print(championInfo)
-
-        
+        # print(championInfo)
 
         champions = json.loads(resp.text)
         print(f"length of summoners: {len(champions)}")
 
-        bpy.ops.object.select_all(action='SELECT') # selektiert alle Objekte
-        bpy.ops.object.delete(use_global=False, confirm=False) # löscht selektierte objekte
-        bpy.ops.outliner.orphans_purge() # löscht überbleibende Meshdaten etc.  
+        bpy.ops.object.select_all(action='SELECT')  # selektiert alle Objekte
+        # löscht selektierte objekte
+        bpy.ops.object.delete(use_global=False, confirm=False)
+        bpy.ops.outliner.orphans_purge()  # löscht überbleibende Meshdaten etc.
 
         i = 0
-        #for y in range(2):
-        for x in range(6):              
-        #for summoner in summoners:                        
-                
+        # for y in range(2):
+        for x in range(6):
+            # for summoner in summoners:
+
             total_height = 0
             rand_offset = 0.4
 
-            """ currentchamp = ""
-            for champ in range(len(championInfo)):
-                if championInfo['data'][champ]['key'] == champions[i]['championId']:
-                    currentchamp = championInfo['data'][champ]['key'] """
+            currentchamp = ""
+            for key in championInfo["data"]:
+                # print(championInfo['data'][key]['id'])
+                if str(championInfo['data'][key]['key']) == str(champions[i]["championId"]):
+                    currentchamp = str(championInfo['data'][key]['name'])
+                    print("current champ: ")
+                    print(currentchamp)
+                    break
 
-            bpy.data.curves.new(type="FONT", name=f"Font Curve{i}").body = str(champions[i]['championId'])
-            font_obj = bpy.data.objects.new(name=f"Font Object{i}", object_data=bpy.data.curves[f"Font Curve{i}"])
+            bpy.data.curves.new(
+                type="FONT", name=f"Font Curve{i}").body = currentchamp
+            font_obj = bpy.data.objects.new(
+                name=f"Font Object{i}", object_data=bpy.data.curves[f"Font Curve{i}"])
             bpy.context.scene.collection.objects.link(font_obj)
-            font_obj.location = (x*5-17,-3,0)
-            font_obj.rotation_euler = (45,0,0)
+            font_obj.location = (x*5-17, -3, 0)
+            font_obj.rotation_euler = (45, 0, 0)
 
             for z in range(int(champions[i]['championPoints']/1000)):
-                c_cube_size = random.uniform(self.cube_size_min,self.cube_size_max)
-                
-                bpy.ops.mesh.primitive_cube_add(location=(x*5-17,0, total_height + c_cube_size/2),size=(c_cube_size))
-                total_height+= c_cube_size
-                
-                
-                bpy.context.object.rotation_euler.z = random.uniform(0, 360)
-                
-                bpy.context.object.location.x += random.uniform(-c_cube_size*rand_offset, +c_cube_size*rand_offset)
-                bpy.context.object.location.y += random.uniform(-c_cube_size*rand_offset, +c_cube_size*rand_offset)
-                bpy.ops.rigidbody.object_add()
-                bpy.context.object.color = (self.cube_color.r,self.cube_color.g,self.cube_color.b,random.uniform(0.5,1))
+                c_cube_size = random.uniform(
+                    self.cube_size_min, self.cube_size_max)
 
-            #print("help")
-            
+                bpy.ops.mesh.primitive_cube_add(
+                    location=(x*5-17, 0, total_height + c_cube_size/2), size=(c_cube_size))
+                total_height += c_cube_size
+
+                bpy.context.object.rotation_euler.z = random.uniform(0, 360)
+
+                bpy.context.object.location.x += random.uniform(
+                    -c_cube_size*rand_offset, +c_cube_size*rand_offset)
+                bpy.context.object.location.y += random.uniform(
+                    -c_cube_size*rand_offset, +c_cube_size*rand_offset)
+                bpy.ops.rigidbody.object_add()
+                bpy.context.object.color = (
+                    self.cube_color.r, self.cube_color.g, self.cube_color.b, random.uniform(0.5, 1))
+
+            # print("help")
 
             print(f"i: {i}")
             print(f"ChPoints: {champions[i]['championPoints']}")
@@ -181,24 +187,26 @@ class APIAddon(bpy.types.Operator):
             print("")
             i += 1
 
-            
         bpy.ops.mesh.primitive_plane_add(size=30)
         bpy.ops.rigidbody.object_add()
         bpy.context.object.rigid_body.type = 'PASSIVE'
 
-
         return {"FINISHED"}
+
 
 def menu_func(self, context):
     self.layout.operator(APIAddon.bl_idname, icon="SELECT_EXTEND")
 
+
 def register():
     bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
     bpy.utils.register_class(APIAddon)
-    
+
+
 def unregister():
     bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
     bpy.utils.unregister_class(APIAddon)
-        
+
+
 if __name__ == "__main__":
     register()
